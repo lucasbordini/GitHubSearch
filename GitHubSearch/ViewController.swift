a210746072ff534c827e8eafdf44709ef76c0553
Lucas <lucas.araujo-ext@ab-inbev.com>
2019-07-17 11:46:29 -0300

    Initial Commit

---
+++ GitHubSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubSearch
+//
+//  Created by mazzatech on 17/07/19.
+//  Copyright © 2019 lucasbordini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
